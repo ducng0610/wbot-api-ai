bb1a95b761902caa07b942ffef45a50156fbe4f4
Duc Anh Nguyen <duc4nh@Ducs-MacBook-Pro.local>
2017-02-02 17:31:06 +0700

    Bot config for message postbacks

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -7,12 +7,32 @@ include Facebook::Messenger
 
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV['ACCESS_TOKEN'])
 
-# message.id          # => 'mid.1457764197618:41d102a3e1ae206a38'
-# message.sender      # => { 'id' => '1008372609250235' }
-# message.sent_at     # => 2016-04-22 21:30:36 +0200
-# message.text        # => 'Hello, bot!'
-
 Bot.on :message do |message|
+  # message.id          # => 'mid.1457764197618:41d102a3e1ae206a38'
+  # message.sender      # => { 'id' => '1008372609250235' }
+  # message.sent_at     # => 2016-04-22 21:30:36 +0200
+  # message.text        # => 'Hello, bot!'
+
   puts "[debuz] got from Facebook... #{message.text}"
   ChatExtension.response(message.text, message.sender['id'])
+end
+
+Bot.on :postback do |postback|
+  # postback.sender    # => { 'id' => '1008372609250235' }
+  # postback.recipient # => { 'id' => '2015573629214912' }
+  # postback.sent_at   # => 2016-04-22 21:30:36 +0200
+  # postback.payload   # => 'EXTERMINATE'
+
+  if postback.payload == 'DEVELOPER_DEFINED_PAYLOAD_FOR_HELP'
+    puts "[debuz] Human #{postback.recipient} marked for extermination"
+    Bot.deliver(
+      {
+        recipient: postback.sender,
+        message: {
+          text: 'I can tell you the (1) Current Weather (2) 24-Hour Forecast (3) PSI  in different locations in Singapore. For example, you can ask me "What is the weather in Changi?"; "Is it raining in Bedok?"; "What is the forecast in the South?" or "What is the PSI in the North?"'
+        }
+      },
+      access_token: ENV['ACCESS_TOKEN']
+    )
+  end
 end
