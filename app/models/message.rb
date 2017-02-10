0b0971e342792d865ba871eaa6bb740032394cbd
Duc Anh Nguyen <duc4nh@Ducs-MacBook-Pro.local>
2017-02-10 11:05:18 +0700

    bug/ handle exception on sending message to non-facebook uid

--- app/models/message.rb
+++ app/models/message.rb
@@ -35,14 +35,18 @@ class Message
   private
 
   def deliver(response_message, uid)
-    Bot.deliver(
-      {
-        recipient: {
-          id: uid
+    begin
+      Bot.deliver(
+        {
+          recipient: {
+            id: uid
+          },
+          message: response_message
         },
-        message: response_message
-      },
-      access_token: ENV['ACCESS_TOKEN']
-    )
+        access_token: ENV['ACCESS_TOKEN']
+      )
+    rescue => e
+      puts '[debuz] ' + e.message
+    end
   end
 end
