ced6f258f76250f3863725f6b5c04510e06430e0
Duc Anh Nguyen <duc4nh@Ducs-MacBook-Pro.local>
2017-04-13 11:28:14 +0700

    deploy/ update config

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :linked_files, %w{config/mongoid.yml config/application.yml}
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
